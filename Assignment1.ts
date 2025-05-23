9f8689e36e2cf6a79239f06864eb72c35a75c6a8
Your Name <you@example.com>
2025-05-23 19:57:26 +0600

    problem 2 solve

--- Assignment1.ts
+++ Assignment1.ts
@@ -1,7 +1,6 @@
 {
     //
     function formatString(input: string, toUpper?: boolean): string {
-
         if (toUpper === true) {
             return input.toUpperCase();
         }
@@ -17,6 +16,22 @@
     console.log(formatString('Hello'));
     console.log(formatString("Hello", true));
     console.log(formatString("Hello", false));
+
+    /*********************************************************** */
+
+    function filterByRating(items: { title: string; rating: number; }[]): { title: string; rating: number; }[] {
+        return items.filter(selectedItem => selectedItem.rating >= 4);
+    }
+
+    const books = [
+        { title: "Book A", rating: 4.5 },
+        { title: "Book B", rating: 3.2 },
+        { title: "Book C", rating: 5.0 },
+        { title: "Book D", rating: 3.9 },
+        { title: "Book E", rating: 4 }
+    ];
+
+    console.log(filterByRating(books));
 
     //**Finish */
 }
