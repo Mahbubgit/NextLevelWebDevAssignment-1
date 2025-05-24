743707a1c3dc746eaa2b14ff19e20b2151508472
Your Name <you@example.com>
2025-05-24 07:26:19 +0600

    problem 7 solved

--- Assignment1.ts
+++ Assignment1.ts
@@ -130,5 +130,30 @@
     console.log(getMostExpensiveProduct(products));
     console.log(getMostExpensiveProduct(emptyProduct));
 
+    /*********************************************************** */
+
+    enum Day {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+    };
+
+    function getDayType(day: Day): string {
+        if (day === Day.Sunday) {
+            return 'Weekend';
+        }
+        else {
+            return 'Weekday';
+        }
+    };
+
+    console.log(getDayType(Day.Monday));
+    console.log(getDayType(Day.Sunday));
+    console.log(getDayType(Day.Thursday));
+
     //**Finish **/
 }
